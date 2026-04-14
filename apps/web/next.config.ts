e37c13b698d99db3cf4afd1b9f529309c810ab36
cokonnect25-svg <cokonnect25@gmail.com>
2026-04-14 20:53:08 +0530

    initial web appevrgffvhgfhdsvawfghc ddsefascdfb

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   trailingSlash: true,
-  distDir: 'out',
   devIndicators: { buildActivity: false },
   images: {
     unoptimized: true,
