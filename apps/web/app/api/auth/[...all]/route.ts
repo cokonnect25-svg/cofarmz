5f3b1a331d332626e86c52d0b9afaffd2d47f97a
nivethap16 <nivethacpt16@gmail.com>
2026-04-15 17:20:29 +0530

    dvbgdfhgzdvascdvdvghjerftgyfghasc dsdvsdkdshviudvdascSCghjkafe

--- apps/web/app/api/auth/[...all]/route.ts
+++ apps/web/app/api/auth/[...all]/route.ts
@@ -2,6 +2,4 @@ export const dynamic = 'force-dynamic';
 import { auth } from "@/lib/auth";
 import { toNextJsHandler } from "better-auth/next-js";
 
-export const {GET, POST } = toNextJsHandler(auth);
-
-//https://cofarmz.vercel.app/api/auth/[...all]//
+export const { GET, POST } = toNextJsHandler(auth);
